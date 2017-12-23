86b8e3ebb0b451fdb1b357ed860e18e8bbe82e37
Himank Goel <himankgoel03@gmail.com>
2017-12-23 16:47:59 +0530

    Added Routes for SignIn, SignUp and App Pages

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, browserHistory } from 'react-router';  
 import App from './components/App';
 import SignIn from './components/SignIn';
 import SignUp from './components/SignUp';
 
 ReactDOM.render(
-    <div> App </div>, document.getElementById('root')
+    <Router path = "/" history = {browserHistory}>
+        <Route path = "/app" component = {App} />
+        <Route path = "/signin" component = {SignIn} />
+        <Route path = "/signup" component = {SignUp} />
+    </Router>, document.getElementById('root')
 )

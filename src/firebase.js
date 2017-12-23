89f0f656fc005eb9e294ccbec754cbb0e7d066bc
Himank Goel <himankgoel03@gmail.com>
2017-12-23 17:14:21 +0530

    Added Firebase Authentication

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import * as firebase from 'firebase'
+
+const config = {
+    apiKey: "AIzaSyCoPiCpEqQo9ZPkziJkrqxpPw9XrKdLghA",
+    authDomain: "goalcoach-78340.firebaseapp.com",
+    databaseURL: "https://goalcoach-78340.firebaseio.com",
+    projectId: "goalcoach-78340",
+    storageBucket: "goalcoach-78340.appspot.com",
+    messagingSenderId: "41445809351"
+};
+
+export const firebaseApp = firebase.initializeApp(config);
--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, browserHistory } from 'react-router';  
+import { firebaseApp } from './firebase';
 import App from './components/App';
 import SignIn from './components/SignIn';
 import SignUp from './components/SignUp';
+
+firebaseApp.auth().onAuthStateChanged(user => {
+    if(user) {
+        console.log('user has signed in or up', user);  
+    }
+    else {
+        console.log('out');
+    }
+})
 
 ReactDOM.render(
     <Router path = "/" history = {browserHistory}>

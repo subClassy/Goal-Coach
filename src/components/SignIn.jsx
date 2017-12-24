938cc5cb24cbaea926b0930eaacc6baa7a02fca9
Himank Goel <himankgoel03@gmail.com>
2017-12-24 13:02:16 +0530

    UI for signin component

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,9 +1,67 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router';
+import { firebaseApp } from '../firebase';
 
 class SignIn extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            password: '',
+            error: {
+                message: ''
+            }
+        }
+    }
+
+    signIn() {
+        console.log(this.state);
+        const {email, password} = this.state;
+        firebaseApp.auth().signInWithEmailAndPassword(email, password)
+            .catch(error => {
+                console.log(error);
+                this.setState({error});
+            }) 
+    }
+
     render() {
         return (
-            <div>SignIn</div>
+            <div className = "form-inline" style = {{margin: '5%'}}>
+                <h2> Sign In</h2>
+                <div className = "form-group">
+                    <input
+                        className = "form-control"
+                        type = "text"
+                        style = {{marginRight: '5px'}}
+                        placeholder = "email"
+                        onChange = {
+                            event => this.setState({
+                                email: event.target.value
+                            })
+                        }
+                    />
+                    <input
+                        className = "form-control"
+                        type = "password"
+                        style = {{marginRight: '5px'}}
+                        placeholder = "password"
+                        onChange = {
+                            event => this.setState({
+                                password: event.target.value
+                            })
+                        }
+                    />
+                    <button
+                        className = "btn btn-primary"
+                        type = "button"
+                        onClick = {() => this.signIn()}
+                    >
+                        Sign Up
+                    </button>
+                </div>
+                <div>{this.state.error.message}</div>
+                <div><Link to = {'/signup'}>SignUp instead</Link></div>
+            </div>
         )
     }
 }
--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router';
 import { firebaseApp } from '../firebase';
 
 class SignUp extends Component {
@@ -59,6 +60,7 @@ class SignUp extends Component {
                     </button>
                 </div>
                 <div>{this.state.error.message}</div>
+                <div><Link to = {'/signin'}>Already a user ? Sign in instead</Link></div>
             </div>
         )
     }

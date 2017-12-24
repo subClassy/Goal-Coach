895945868babe1f973e14f71d654d3ad582441f8
Himank Goel <himankgoel03@gmail.com>
2017-12-24 12:51:53 +0530

    UI for signup component

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,9 +1,65 @@
 import React, { Component } from 'react';
+import { firebaseApp } from '../firebase';
 
 class SignUp extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            password: '',
+            error: {
+                message: ''
+            }
+        }
+    }
+
+    signUp() {
+        console.log(this.state);
+        const {email, password} = this.state;
+        firebaseApp.auth().createUserWithEmailAndPassword(email, password)
+            .catch(error => {
+                console.log(error);
+                this.setState({error});
+            }) 
+    }
+
     render() {
         return (
-            <div>SignUp</div>
+            <div className = "form-inline" style = {{margin: '5%'}}>
+                <h2> Sign Up</h2>
+                <div className = "form-group">
+                    <input
+                        className = "form-control"
+                        type = "text"
+                        style = {{marginRight: '5px'}}
+                        placeholder = "email"
+                        onChange = {
+                            event => this.setState({
+                                email: event.target.value
+                            })
+                        }
+                    />
+                    <input
+                        className = "form-control"
+                        type = "password"
+                        style = {{marginRight: '5px'}}
+                        placeholder = "password"
+                        onChange = {
+                            event => this.setState({
+                                password: event.target.value
+                            })
+                        }
+                    />
+                    <button
+                        className = "btn btn-primary"
+                        type = "button"
+                        onClick = {() => this.signUp()}
+                    >
+                        Sign Up
+                    </button>
+                </div>
+                <div>{this.state.error.message}</div>
+            </div>
         )
     }
 }

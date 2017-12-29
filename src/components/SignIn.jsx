b53fc1ee77cd7330973fae952f763f6179f4b6ca
Himank Goel <himankgoel03@gmail.com>
2017-12-29 23:40:16 +0530

    Improved UI

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Container, Box, Columns, Column } from 'bloomer';
 import { Link } from 'react-router';
 import { firebaseApp, auth, providerGoogle, providerFacebook } from '../firebase';
 import '../App.css';
@@ -58,58 +59,64 @@ class SignIn extends Component {
         return (
             <div className = "Sign-In-Form">
                 <h1>Goal Coach</h1>
-                <div className = "form-horizontal SignIn-Form">
-                    <div className = "Form">
-                        <h2> Sign In </h2>
-                        <div className = "form-group">
-                            <input
-                                className = "form-control"
-                                type = "text"
-                                placeholder = "email"
-                                onChange = {
-                                    event => this.setState({
-                                        email: event.target.value
-                                    })
-                                }
-                            />
-                            <input
-                                className = "form-control"
-                                type = "password"
-                                placeholder = "password"
-                                onChange = {
-                                    event => this.setState({
-                                        password: event.target.value
-                                    })
-                                }
-                            />
-                            <button
-                                className = "btn btn-primary"
-                                type = "button"
-                                onClick = {() => this.signIn()}
-                            >
-                                Sign In
-                            </button>
-                            <button
-                                className = "btn btn-danger"
-                                type = "button"
-                                onClick = {() => this.handleGoogleLogin()}
-                                style = {{marginLeft: '5px'}}
-                            >
-                                Sign In With Google
-                            </button>
-                            <button
-                                className = "btn btn-primary"
-                                type = "button"
-                                onClick = {() => this.handleFacebookLogin()}
-                                style = {{marginLeft: '5px'}}
-                            >
-                                Sign In With Facebook
-                            </button>
-                        </div>
-                        <div>{this.state.error.message}</div>
-                        <div><Link to = {'/signup'}>SignUp instead</Link></div>
-                    </div>
-                </div>
+                <Columns isCentered>
+                    <Column isSize = {{mobile: 8, desktop: 1/3}}>
+                        <Container isFluid>
+                            <Box className = "SignIn-Box">
+                                <div className = "form-horizontal">
+                                    <h2> Sign In </h2>
+                                    <div className = "form-group">
+                                        <input
+                                            className = "form-control"
+                                            type = "text"
+                                            placeholder = "email"
+                                            onChange = {
+                                                event => this.setState({
+                                                    email: event.target.value
+                                                })
+                                            }
+                                        />
+                                        <input
+                                            className = "form-control"
+                                            type = "password"
+                                            placeholder = "password"
+                                            onChange = {
+                                                event => this.setState({
+                                                    password: event.target.value
+                                                })
+                                            }
+                                        />
+                                        <button
+                                            className = "btn btn-primary"
+                                            type = "button"
+                                            onClick = {() => this.signIn()}
+                                        >
+                                            Sign In
+                                        </button>
+                                        <button
+                                            className = "btn btn-danger"
+                                            type = "button"
+                                            onClick = {() => this.handleGoogleLogin()}
+                                            style = {{marginLeft: '5px'}}
+                                        >
+                                            Sign In With Google
+                                        </button>
+                                        <button
+                                            className = "btn btn-primary"
+                                            type = "button"
+                                            onClick = {() => this.handleFacebookLogin()}
+                                            style = {{marginLeft: '5px'}}
+                                        >
+                                            Sign In With Facebook
+                                        </button>
+                                    </div>
+                                    <div>{this.state.error.message}</div>
+                                    <div><Link to = {'/signup'}>SignUp instead</Link></div>
+                                </div>
+                            </Box>
+                        </Container>
+                    </Column>
+                </Columns>
             </div>
         )
     }

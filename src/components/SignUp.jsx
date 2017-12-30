0b24af94823115556a5dd30298e3a33b73a149ff
Himank Goel <himankgoel03@gmail.com>
2017-12-30 21:38:16 +0530

    Improved SignUp UI

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
 import { firebaseApp } from '../firebase';
+import { Columns } from 'bloomer/lib/grid/Columns';
+import { Column } from 'bloomer/lib/grid/Column';
+import { Box } from 'bloomer/lib/elements/Box';
+import { Label } from 'bloomer/lib/elements/Form/Label';
 
 class SignUp extends Component {
     constructor(props) {
@@ -35,42 +39,51 @@ class SignUp extends Component {
 
     render() {
         return (
-            <div className = "form-inline" style = {{margin: '5%'}}>
-                <h2> Sign Up</h2>
-                <div className = "form-group">
-                    <input
-                        className = "form-control"
-                        type = "text"
-                        style = {{marginRight: '5px'}}
-                        placeholder = "email"
-                        onChange = {
-                            event => this.setState({
-                                email: event.target.value
-                            })
-                        }
-                    />
-                    <input
-                        className = "form-control"
-                        type = "password"
-                        style = {{marginRight: '5px'}}
-                        placeholder = "password"
-                        onChange = {
-                            event => this.setState({
-                                password: event.target.value
-                            })
-                        }
-                    />
-                    <button
-                        className = "btn btn-primary"
-                        type = "button"
-                        onClick = {() => this.signUp()}
-                    >
-                        Sign Up
-                    </button>
-                </div>
-                <div>{this.state.error.message}</div>
-                <div><Link to = {'/signin'}>Already a user ? Sign in instead</Link></div>
-            </div>
+            <Columns className = "Sign-Up-Form">
+                <h1> Goal Coach </h1>
+                <Column className = "signup-form">
+                    <Box className = "signup-box">
+                        <div className = "form-horizontal">
+                            <h2>Sign Up</h2>
+                            <hr />
+                            <div className = "form-group">
+                                <Label className = "label">Email</Label>
+                                <input
+                                    className = "form-control email-input"
+                                    type = "text"
+                                    onChange = {
+                                        event => this.setState({
+                                            email: event.target.value
+                                        })
+                                    }
+                                />
+                                <Label className = "label">Password</Label>
+                                <input
+                                    className = "form-control password-input"
+                                    type = "password"
+                                    onChange = {
+                                        event => this.setState({
+                                            password: event.target.value
+                                        })
+                                    }
+                                />
+                                <button
+                                    className = "btn signup-btn"
+                                    type = "button"
+                                    onClick = {() => this.signUp()}
+                                >
+                                    SIGN UP
+                                </button>
+                            </div>
+                            {
+                                (this.state.error.message !== '') ? alert(this.state.error.message) : <p></p>
+                            }
+                            <hr />
+                            <p className = "signin-link"><Link to = {'/signin'} className = "link">Already a user ? Sign in instead</Link></p>
+                        </div>
+                    </Box>
+                </Column>
+            </Columns>
         )
     }
 }

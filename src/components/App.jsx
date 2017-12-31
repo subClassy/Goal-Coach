6e5c08fae644f6acc3d6a7579f29e45e04c89cdd
Himank Goel <himankgoel03@gmail.com>
2017-12-31 19:10:53 +0530

    Improved UI of App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,6 +4,8 @@ import { firebaseApp } from '../firebase';
 import AddGoal from './AddGoal';
 import GoalList from './GoalList';
 import CompleteGoalList from './CompleteGoalList';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+ import '../../node_modules/react-tabs/style/react-tabs.css';
 
 class App extends Component {
     signOut() {
@@ -16,12 +18,32 @@ class App extends Component {
                 <h3>Goal Coach</h3>
                 <AddGoal />
                 <hr />
-                <h4>Goals</h4>
-                <GoalList />
-                <hr />
-                <h4>Complete Goals</h4>
-                <CompleteGoalList />
-                <hr />
+                <Tabs>
+                    <TabList>
+                        <Tab>All</Tab>
+                        <Tab>Incomplete</Tab>
+                        <Tab>Completed</Tab>
+                    </TabList>
+
+                    <TabPanel>
+                        <h4>Goals</h4>
+                        <GoalList />
+                        <hr />
+                        <h4>Complete Goals</h4>
+                        <CompleteGoalList />
+                        <hr />
+                    </TabPanel>
+                    <TabPanel>
+                        <h4>Goals</h4>
+                        <GoalList />
+                        <hr />
+                    </TabPanel>
+                    <TabPanel>
+                        <h4>Complete Goals</h4>
+                        <CompleteGoalList />
+                        <hr />
+                    </TabPanel>
+                </Tabs>
                 <button 
                     className = "btn btn-danger"
                     onClick = {() => this.signOut()}

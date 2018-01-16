7480802635a3ae67b634f08d54d0af41c5e2a1bc
Tlazypanda <33183263+Tlazypanda@users.noreply.github.com>
2018-01-16 21:38:58 +0530

    Fixes #2 Partial Code (#10)
    * Update SignUp.jsx
    
    * Update SignUp.jsx
    
    * Update SignIn.jsx
    
    * Update SignIn.jsx
    
    * Update GoalItem.jsx
    
    * Update GoalItem.jsx

--- src/components/GoalItem.jsx
+++ src/components/GoalItem.jsx
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { completeGoalRef, goalRef } from '../firebase';
+import * as firebase from 'firebase';
 
 class GoalItem extends Component {
     completeGoal() {
         const {email} = this.props.user;
         const {title, serverKey} = this.props.goal;
         goalRef.child(serverKey).remove();
+        var user = firebaseApp.auth().currentUser;
+                console.log(user);
+        var ref=firebase.database().ref('users/' + user.uid);
+        ref.on('value',function(datasnapshot){
+            console.log(datasnapshot.val().username);
+         });
         completeGoalRef.push({email, title});
     }
 
